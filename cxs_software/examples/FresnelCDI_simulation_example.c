4cdff92bf3f3dc095fafbcf2192ce09b6e541547
ndavidson <ndavidson@7ca69b2b-3e8c-f36b-109c-63a388394f7f>
2011-03-18 05:26:18 +0000

    fixed FCDI simulation example

--- cxs_software/examples/FresnelCDI_simulation_example.c
+++ cxs_software/examples/FresnelCDI_simulation_example.c
@@ -9,7 +9,7 @@
  * @author Michael Jones <michael.jones@latrobe.edu.au>,
  * Nadia Davidson <nadiamd@unimelb.edu.au> 
  *
- * Last modified on 22/2/2011
+ * Last modified on 18/3/2011
  *
  */
 
@@ -37,14 +37,9 @@ int main(int argc, char * argv[]){
   int output_iterations = 25;
   int total_iterations = 200;
 
-  //load the object file
+  //load an image file for the sample
   Double_2D object;
-  int status = read_tiff("image_files/FCDI_simulation_object.tiff", object);
-  if(!status){
-    cout<<"failed to get the object "
-	<<"...exiting" <<endl;
-    return(1);
-  }
+  int status = read_image("image_files/FCDI_simulation_object.tiff", object);
 
   //get the object dimensions
   int nx = object.get_size_x();
@@ -52,19 +47,14 @@ int main(int argc, char * argv[]){
 
   //load the support to use in the reconstruction
   Double_2D support;
-  status = read_tiff("image_files/FCDI_simulation_support.tiff", support);
-  if(!status){
-    cout<<"failed to get the support "
-	<<"...exiting" <<endl;
-    return(1);
-  }
+  status = read_image("image_files/FCDI_simulation_support.tiff", support);
   
   //we will use the same white field as the Fresnel examples. For
   //this reason you will need to reconstruct it first using
-  //FresnelCDI_example.
+  //FresnelCDI_WF_example.exe
   Complex_2D wf(nx,ny);
   status = read_cplx("wf_recovered.cplx", wf);
-  if(!status){
+  if(!status){ //give an error if we couldn't open the file.
     cout  << "Maybe you need to run ./FCDI_WF_example.exe "
 	  << "first... exiting"  << endl;
     return(1);
@@ -86,13 +76,13 @@ int main(int argc, char * argv[]){
 
   //make a 2D array and allocate some memory.
   //This will be used to output the image of the 
-  //current estimate.
+  //current exit-surface-wave estimate.
   Double_2D result(nx,ny);
   
-  //set parameters
+  //set the experimental parameters (all are in meters)
   double wavelength = 4.892e-10; //wavelength
-  double fd = 0.909531 - 16.353e-3; //focal to detector
-  double fs = 18.513e-3 - 16.353e-3; //focal to sample
+  double fd = 0.8932; //focal to detector
+  double fs = 2.16e-3; //focal to sample
   double ps = 13.5e-6; //pixel size
 
   //create the projection object which will be used later to
@@ -105,23 +95,19 @@ int main(int argc, char * argv[]){
 		  fd, //focal-to-detector
 		  fs, //focal-to-sample
 		  ps, //pixel size
-		  1.0); //normalisation 
+		  1.0); //normalisation of white-field to sample data
 
-  //******************************************
-  //Project the object to the image plane 
-  //******************************************  
+  //***************************************************
+  //  Simulate a diffraction pattern
+  //***************************************************  
 
   /*** First we will create a transmission function **/
+
   // create the complex array which it will be stored in 
   Complex_2D input(ny,ny);
 
-  //now fill the input with a scaled version of the simulation
-  //object image. This part need to be controlled a bit by
-  //the person doing the simulation. e.g. to work out what
-  //transmission functions are physical for which types of
-  //materials etc. 
-
-  double thickness = 150e-9;   //150nm of gold
+  //lets say our sample is 150nm maximum thickness and is made from gold
+  double thickness = 150e-9;
   double delta = 6.45e-4;
   double beta = 1.43e-4;
   double k = (2.0 * M_PI)/wavelength;
@@ -130,7 +116,9 @@ int main(int argc, char * argv[]){
 
   for (int i = 0; i<nx; i++){
     for (int j = 0; j<ny; j++){
-      
+
+      //calculate the amplitude and phase of the transmission function
+      //at each point (from the image).
       double this_pixel_thickness = thickness*object.get(i,j)/max;
       double mag = exp(-beta*k*this_pixel_thickness);
       double phase = -delta*k*this_pixel_thickness;
@@ -140,13 +128,14 @@ int main(int argc, char * argv[]){
     }
   }
 
+  //save the amplitude and phase to file so we know how it looks
   input.get_2d(MAG,result);
   write_tiff("trans_mag_input.tiff",result);
   input.get_2d(PHASE,result);
   write_tiff("trans_phase_input.tiff",result);
 
-  //now simulate: 
- 
+  /********* now simulate: ***********************************/ 
+   
   //get the illuminating white field in the sample plane
   proj.propagate_from_detector(wf);
   
@@ -158,7 +147,7 @@ int main(int argc, char * argv[]){
   //write the output to file
   write_tiff("sample_plane.tiff",result);
 
-  //propagate to detector
+  //propagate to the detector
   proj.propagate_to_detector(input);
 
   //take the wf back to the detector so we're ready for the reconstructions
@@ -168,9 +157,20 @@ int main(int argc, char * argv[]){
   //"result" will be used as the diffraction data.
   input.get_2d(MAG_SQ,result);
 
-  //write thresholded output to file
+  //write the diffraction pattern to a file
   write_tiff("forward_projection.tiff",result);
 
+  //if you feel like making a more realistic simulation with threasholding
+  //and simulated noise, consider uncommenting the code below.
+  //add random noise to the output. Where, and in what form is best??
+  /**for(int i=0; i<nx; i++){
+     for(int j=0; j<ny; j++){
+        result.set(i,j,result.get(i,j) * (rand()%21) );
+	if(result.get(i,j)<0)
+      	   result.set(i,j,0);
+     }
+     }**/
+  
   //the approx. theshold level of the image.
   //Not too much (less than 1!, depending on absorption level below)
   //const double noise_level = 0.5;
@@ -181,19 +181,9 @@ int main(int argc, char * argv[]){
       result.set(i,j,result.get(i,j)-noise_level);
       if(result.get(i,j)<0)
 	result.set(i,j,0);
-    }
-    }**/
+	}
+	}**/
   
-  /*
-  //add random noise to the output. Where, and in what form is best??
-  for(int i=0; i<nx; i++){
-     for(int j=0; j<ny; j++){
-        result.set(i,j,result.get(i,j) * (rand()%21) );
-	if(result.get(i,j)<0)
-      	   result.set(i,j,0);
-     }
-  }
-  */
   
   /****************************************************/
   /*******  set up the reconstuction ****************
@@ -206,18 +196,13 @@ int main(int argc, char * argv[]){
   //set the support and intensity
   proj.set_support(support);  
   proj.set_intensity(result);
-
-
-  //parameters are the same as before except norm
-  //normalisation between wf and image
-  input.get_2d(MAG,result);
-
+  
   //set the algorithm
   proj.set_algorithm(ER);
-
+  
   //Initialise the current object ESW
   proj.initialise_estimate(0);
-
+  
   //set a complex contraint for the transmission function
   //proj.set_complex_contraint_function(charge_flip);
   
@@ -239,15 +224,17 @@ int main(int argc, char * argv[]){
       temp_str << "fcdi_example_iter_" << i << ".tiff";
       write_tiff(temp_str.str(),result);
     
+      //uncomment to apply the shrinkwrap algorithm
       //      proj.apply_shrinkwrap();
     }
   }
 
-  //get the transmission function for the final iteration
+  //get the reconstructed transmission function for 
+  //the final iteration
   Complex_2D trans(nx,ny);
   proj.get_transmission_function(trans);
 
-  //I can't see this without using log scale.
+  //and write the magnitude and phase of it to an image file
   trans.get_2d(MAG,result);
   write_tiff("trans_mag_recovered.tiff",result);
 
