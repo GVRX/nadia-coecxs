a07fe80acf22f0d0e11bb563abf9e13674caf0d3
ndavidson <ndavidson@7ca69b2b-3e8c-f36b-109c-63a388394f7f>
2011-05-10 08:58:21 +0000

    Adding more stuff which is work in progress

--- cxs_software/src/utils.c
+++ cxs_software/src/utils.c
@@ -15,6 +15,34 @@ using namespace std;
 #define BINS 100
 #define sgn(x) (( x > 0 ) - ( x < 0 ))
 
+double sgrad(Double_2D & image, int i, int j){
+
+  if(i<1 || j<1 
+     || i > (image.get_size_x()-2)
+     || j > (image.get_size_y()-2))
+    return 0;
+
+      //do the x direction
+  double value_x  = -1*image.get(i-1,j-1);
+  value_x -=  2*image.get(i-1,j);
+  value_x -=  1*image.get(i-1,j+1);
+
+  value_x +=  1*image.get(i+1,j-1);
+  value_x +=  2*image.get(i+1,j);
+  value_x +=  1*image.get(i+1,j+1);
+  
+  //do the y direction
+  double value_y  = -1*image.get(i-1,j-1);
+  value_y -=  2*image.get(i,j-1);
+  value_y -=  1*image.get(i+1,j-1);
+
+  value_y +=  1*image.get(i-1,j+1);
+  value_y +=  2*image.get(i,j+1);
+  value_y +=  1*image.get(i+1,j+1);
+  
+  return sqrt(value_x*value_x + value_y*value_y);
+}
+
 void crop(Double_2D & image, Double_2D & new_image, int x_start, int y_start){
   int nx = new_image.get_size_x();
   int ny = new_image.get_size_y();
@@ -55,32 +83,34 @@ void rescale(Double_2D & image, double scale){
       double total_weights=0;
       double weighted_value=0;
 
-      for(double x = (int) i_; x <= i_ + scale; x++ ){
-	for(double y = (int) j_; y <= j_ + scale; y++){
+      for(double x = (int) i_; x < i_ + scale; x++ ){
+	for(double y = (int) j_; y < j_ + scale; y++){
 
 	  //check that we are still in range
-	  if(i_<nx&&i_>=0&&j_<ny&&j_>=0){
+	  if(x<nx&&x>=0&&y<ny&&y>=0){
 
 	    //subtract the remainders from one.
 	    double x_fraction = 1;
 	    double y_fraction = 1;
 
-	    if(x<i_)
+	    if(x < i_)
 	      x_fraction = 1 - fmod(i_, 1.0);
-	    if(y<j_)
+
+	    if(y < j_)
 	      y_fraction = 1 - fmod(j_, 1.0);
 
-	    if(x > i_ + scale)
-	      x_fraction = fmod(i_, 1.0);
+	    if(x > i_ + scale -1)
+	      x_fraction = fmod((i_+scale), 1.0);
 
-	    if(y > j_ + scale)
-	      y_fraction = fmod(j_, 1.0);
+	    if(y > j_ + scale - 1)
+	      y_fraction = fmod((j_+scale), 1.0);
 
 	    //add to the weight
 	    double this_weight = x_fraction*y_fraction;
 
 	    total_weights += this_weight;
 	    weighted_value += this_weight*image.get(x,y);
+
 	  }
 	}
       }
@@ -263,16 +293,20 @@ double simple(Double_2D & image, double scale){
   int nx = image.get_size_x();
   int ny = image.get_size_y();
   
+  Double_2D image_copy(nx,ny);
+  image_copy.copy(image);
+  image_copy.scale(scale);
+
   double total = 0;
 
   //loop over the array once to sort into bins
   for(int i=0; i<nx; i++){
     for(int j=0; j<ny; j++){
-      total+=pow(image.get(i,j),4);
+      total+=pow(image_copy.get(i,j),2);
     }
   }
   
-  return total;
+  return sqrt(total)/(nx*ny);
 }
 
 //no good
@@ -311,13 +345,13 @@ double calculate_image_entropy(Double_2D & image){
   //  double max = 
 
   cout << endl << "a = [";
-  for(int c=0; c < BINS; c++){
+  for(int c=1; c < BINS; c++){
 
     cout << counts[c];
     if(c!=BINS-1)
       cout << ",";
     else
-      cout << "]"<<endl;
+    cout << "]"<<endl;
       
     if(counts[c]!=0){
       //      cout << " log2(counts[c]) = " <<log2(counts[c])<<endl;
@@ -325,10 +359,74 @@ double calculate_image_entropy(Double_2D & image){
     }
   }
   
-  //cout << "here3 "<<entropy << endl;
-  return entropy;
+  //  cout << "Zeros: "<< counts[0] << endl;
+  return counts[2]; //entropy;
 
 }
+
+
+double calculate_image_entropy_2(Double_2D & image){
+  
+  double max = image.get_max();
+  double min = image.get_min();
+  double counts[BINS][BINS]={0};
+  int bin_x;
+  int bin_y;
+
+  int nx = image.get_size_x();
+  int ny = image.get_size_y();
+
+  double total = 2*(nx*ny-1);
+
+  //loop over the array once to sort into bins
+  for(int i=1; i<nx; i++){
+    for(int j=1; j<ny; j++){
+
+      if(image.get(i,j)==max)
+	bin_x = BINS-1;
+      else
+	bin_x = BINS*((image.get(i,j)-min)/(max-min));
+
+      if(image.get(i-1,j)==max)
+	bin_y = BINS-1;
+      else
+	bin_y = BINS*((image.get(i-1,j)-min)/(max-min));
+
+      counts[bin_x][bin_y]+=1.0/total;
+
+      if(image.get(i,j-1)==max)
+	bin_y = BINS-1;
+      else
+	bin_y = BINS*((image.get(i,j-1)-min)/(max-min));
+
+      counts[bin_x][bin_y]+=1.0/total;
+
+      //      cout << "x,y="<<bin_x<<","<<bin_y<<" counts="<<counts[bin_x][bin_y]<< endl;
+
+    }
+  }
+
+  //loop over the bins to get the entropy
+  double entropy = 0;
+
+  for(int c_x=0; c_x < BINS; c_x++){
+    for(int c_y=0; c_y < c_x+1; c_y++){
+      
+      double c = counts[c_x][c_y] + counts[c_y][c_x];
+
+
+      
+      if(c!=0.0&&c_x!=0&&c_y!=0){
+	//cout << "cx="<<c_x<<" cy="<<c_y<<" c="<<c<<endl;
+	entropy-= c*log2(c);
+      }
+      
+    }
+  }
+
+  return entropy;
+}
+
 
 double laplace_gradient(Double_2D & image){
 
@@ -455,21 +553,22 @@ void convolve(Double_2D & array, double gauss_width,
 
 double sobel_gradient(Double_2D & image){
 
-  image.scale(image.get_sum());
+  int nx = image.get_size_x();
+  int ny = image.get_size_y();
 
-  convolve(image,3.0,4);
+  Double_2D mask(nx,ny);
+  mask.copy(image);
+  mask.scale(1.0/image.get_sum());
+  
+  convolve(mask,3.0,9);
 
   char buf[50];
   static int counter = 0;
-  sprintf(buf,"image_conv_%i.tiff",counter);
-  write_image(buf,image);
+  //sprintf(buf,"image_conv_%i.tiff",counter);
+  //write_image(buf,image); 
 
-  double total = 0;
   double value_x ;
   double value_y;
-
-  int nx = image.get_size_x();
-  int ny = image.get_size_y();
 
   // double max = image.get_max()-image.get_min();
   //  threshold = pow(threshold*max,2);
@@ -477,97 +576,98 @@ double sobel_gradient(Double_2D & image){
   //  double min_allowed = threshold*4*max*max;
 
   Double_2D output(nx,ny);
+  Double_2D direction(nx,ny);
 
-  double max = 0;
   double smallest_max = 0;
   double no_of_max = 2;
   double max_total = 0;
 
-  //copy image to the in array
   for(int i=2; i < nx-2 ; i++){
     for(int j=2; j < ny-2 ; j++){
 
       //do the x direction
-      value_x  = -1*image.get(i-1,j-1);
-      value_x -=  2*image.get(i-1,j);
-      value_x -=  1*image.get(i-1,j+1);
+      value_x  = -1*mask.get(i-1,j-1);
+      value_x -=  2*mask.get(i-1,j);
+      value_x -=  1*mask.get(i-1,j+1);
 
-      //    value_x -=  1*image.get(i-2,j);
-
-      value_x +=  1*image.get(i+1,j-1);
-      value_x +=  2*image.get(i+1,j);
-      value_x +=  1*image.get(i+1,j+1);
-
-      //      value_x -=  1*image.get(i+2,j);
+      value_x +=  1*mask.get(i,j-1);
+      value_x +=  2*mask.get(i,j);
+      value_x +=  1*mask.get(i,j+1);
 
       //do the y direction
-      value_y  = -1*image.get(i-1,j-1);
-      value_y -=  2*image.get(i,j-1);
-      value_y -=  1*image.get(i+1,j-1);
+      value_y  = -1*mask.get(i-1,j-1);
+      value_y -=  2*mask.get(i,j-1);
+      value_y -=  1*mask.get(i+1,j-1);
 
-      //      value_y -=  1*image.get(i,j-2);
-
-      value_y +=  1*image.get(i-1,j+1);
-      value_y +=  2*image.get(i,j+1);
-      value_y +=  1*image.get(i+1,j+1);
-
-      // value_x = image.get(i-1,j-1) - image.get(i,j); 
-      //value_y = image.get(i-1,j) - image.get(i,j-1); 
-
-      //      value_y +=  1*image.get(i,j+2);
+      value_y +=  1*mask.get(i-1,j);
+      value_y +=  2*mask.get(i,j);
+      value_y +=  1*mask.get(i+1,j);
 
       double value_total = sqrt(value_x*value_x + value_y*value_y);
-
       output.set(i,j,value_total);
-      total += value_total;
-
-      if(max<value_total){
-	smallest_max = max;
-	max = value_total;
-      }
-      else if(smallest_max<value_total){
-	smallest_max = value_total;
-      }
     }
+
   }
 
-  max_total = (max + smallest_max)/2.0;
   
-  double threshold = 0.5;
-  double mean = total/(double)(nx*ny);
-  cout << "mean is: " << mean << endl;
-  cout << "max_combined is: " << max_total << endl;
-  //double max = output.get_max()<<endl;
+  double max = output.get_max();
+  double cut = 0.2*max;
+  double total = 0;
 
-  //now threshold 
-  /**  double total_t=0.0;
-  double number=0.0;
-  for(int i=0; i < nx ; i++){
-    for(int j=0; j < ny ; j++){
-      if(output.get(i,j) < mean){ //max*threshold)
-	output.set(i,j,0);
-      }
-      else{
-	total_t+=(output.get(i,j)-mean)/(max-mean);
-	number++;
-      }
+  for(int i=2; i < nx-2 ; i++){
+    for(int j=2; j < ny-2 ; j++){
+
+	//do the x direction
+	value_x  = -1*image.get(i-1,j-1);
+	value_x -=  2*image.get(i-1,j);
+	value_x -=  1*image.get(i-1,j+1);
+      
+	value_x +=  1*image.get(i+1,j-1);
+	value_x +=  2*image.get(i+1,j);
+	value_x +=  1*image.get(i+1,j+1);
+
+	//do the y direction
+	value_y  = -1*image.get(i-1,j-1);
+	value_y -=  2*image.get(i,j-1);
+	value_y -=  1*image.get(i+1,j-1);
+	
+	value_y +=  1*image.get(i-1,j+1);
+	value_y +=  2*image.get(i,j+1);
+	value_y +=  1*image.get(i+1,j+1);
+	  
+	//value_x =  image.get(i-1,j-1)-image.get(i,j);
+	//value_y =  image.get(i,j-1)-image.get(i-1,j);
+
+	double value_total = sqrt(value_x*value_x + value_y*value_y);
+	
+	if(output.get(i,j)>cut){
+	  mask.set(i,j,value_total);
+	  direction.set(i,j,atan2(value_y,value_x));
+	}
+	else{
+	  mask.set(i,j,0);
+	  direction.set(i,j,0);
+	}
+	
+	total+=value_total;
     }
   }
 
-  cout << "count is: " << number << endl;
 
 
-  cout << "max is: " << max << endl;
-  cout << "normalised max: " << max_total/image.get_sum() << endl; **/
+  //  cout << "count is: " << number << endl;
 
-  sprintf(buf,"sobel_%i.tiff",counter);
-  write_image(buf,output);
+
+  //  cout << "max is: " << max << endl;
+  //  cout << "normalised max: " << max_total/image.get_sum() << endl;
+
+  sprintf(buf,"sobel_direction_%i.tiff",counter);
+  write_image(buf,direction);
   counter++;
 
-  /**
-  cout << "Entropy of gradient is :"<< calculate_image_entropy(output)<<endl;**/
+  //  cout << "Entropy of gradient is :"<< calculate_image_entropy(output)<<endl;
 
-  return output.get_max();
+  return total; //total_t; // output.get_max();
 
 }
 
@@ -588,7 +688,7 @@ double calculate_gradients(Double_2D & image, double threshold){
   double max = image.get_max()-image.get_min();
   //threshold = pow(threshold*max,2);
 
-  double min_allowed = threshold*4*max*max;
+  double min_allowed = threshold*2*max*max;
 
   max = 0;
 
@@ -596,12 +696,18 @@ double calculate_gradients(Double_2D & image, double threshold){
   //copy image to the in array
   for(int i=1; i < nx-1 ; i++){
     for(int j=1; j < ny-1 ; j++){
-      value = pow(fabs(image.get(i,j)-image.get(i-1,j)),2);      
+      /**      value =pow(fabs(image.get(i,j)-image.get(i-1,j)),2);      
       value+=pow(fabs(image.get(i,j)-image.get(i,j-1)),2);
-      value+= pow(fabs(image.get(i,j)-image.get(i+1,j)),2);      
-      value+=pow(fabs(image.get(i,j)-image.get(i,j+1)),2);
+      value+=pow(fabs(image.get(i,j)-image.get(i+1,j)),2);      
+      value+=pow(fabs(image.get(i,j)-image.get(i,j+1)),2);**/
 
-      output.set(i,j,value);
+      value =fabs( pow(image.get(i,j)-image.get(i-1,j),2 ) );     
+      //		   pow(image.get(i,j)-image.get(i+1,j),3) );
+      
+      value+= fabs( pow(image.get(i,j)-image.get(i,j-1),2 ) );     
+		    //		    pow(image.get(i,j)-image.get(i,j+1),3) );
+
+      output.set(i,j,fabs(value));
       total+=value;
 
       if(value> max)
@@ -612,7 +718,7 @@ double calculate_gradients(Double_2D & image, double threshold){
   Double_2D output_2(nx,ny);
 
   value = 0;
-  //  total = 0;
+  total = 0;
   double total_t = 0;
 
   //now see how isolated they are.
@@ -621,7 +727,7 @@ double calculate_gradients(Double_2D & image, double threshold){
       value=0;
       //  total+=output.get(i,j);
       if(output.get(i,j)>min_allowed){
-	//	total++;
+	total++;
 	value+=output.get(i-1,j);///(max);
 	value+=output.get(i+1,j);///(max);
 	value+=output.get(i,j-1);///(max);
@@ -638,9 +744,9 @@ double calculate_gradients(Double_2D & image, double threshold){
 
 	total_t+=value;
       }
-
+      
       output_2.set(i,j,value);
-
+      
     }
   }
 
@@ -650,7 +756,7 @@ double calculate_gradients(Double_2D & image, double threshold){
   write_image(buf,output_2);
   counter++;
 
-  return total_t/total;
+  return total; //total_t;///total;
 
 }
 
@@ -677,16 +783,19 @@ double vollaths_5(Double_2D & image){
   int nx = image.get_size_x();
   int ny = image.get_size_y();
 
-  double sum = 0;
+  double sum_x = 0;
+  double sum_y = 0;
   double total = 0;
 
   for(int i=1; i < nx ; i++){
-    for(int j=0; j < ny ; j++){
-      sum += image.get(i,j)*image.get(i-1,j);
+    for(int j=1; j < ny ; j++){
+      sum_x += image.get(i,j)*image.get(i-1,j);
+      sum_y += image.get(i,j)*image.get(i,j-1);
       total += image.get(i,j)*image.get(i,j);
     }
   }
-  return sum-(total*total);
+
+  return sum_x*sum_y*total/(pow(nx*ny,3));
 }
 
 double line_out(Double_2D & image){
@@ -716,7 +825,122 @@ double line_out(Double_2D & image){
   return 0;  
 }
 
+double edge_grad(Double_2D & image, Double_2D & mask){
 
+  int nx = image.get_size_x();
+  int ny = image.get_size_y();
+  
+  double value = 0;
+  double edge_points = 0;
+
+  for(int x=1 ; x < nx ; x++){
+    for(int y=1 ; y < ny ; y++){
+      
+      if(mask.get(x,y)!=0){
+	value += sgrad(image,x,y);
+	edge_points++;
+      }
+    }
+  }
+  return value/edge_points;
+}
+
+
+double edges(Double_2D & image){
+
+  int nx = image.get_size_x();
+  int ny = image.get_size_y();
+  
+  Double_2D temp(nx,ny);
+  
+  double threshold = 0.05*image.get_max();
+  double value = 0;
+  double edge_points = 0;
+
+  for(int x=1 ; x < nx ; x++){
+
+    bool edge_found = false;
+    
+    for(int y=1 ; y < ny ; y++){
+
+      if(edge_found==false && image.get(x,y)>threshold ){
+	edge_found = true;
+	//image.set(x,y,60000);
+	if(temp.get(x,y)==0.0){
+	  value += sgrad(image,x,y); //fabs(image.get(x,y)-image.get(x-1,y));
+	  temp.set(x,y,1.0);
+	  edge_points++;
+	}
+      }
+    }
+
+    edge_found = false;
+    
+    for(int y=ny-1 ; y > 0 ; y--){
+      
+      //if(edge_found==true && image.get(x,y)<threshold){ 
+      //edge_found = false;
+      if(edge_found==false && image.get(x,y)>threshold ){
+      	edge_found = true;
+	//image.set(x,y,60000);
+	if(temp.get(x,y)==0.0){
+	  value += sgrad(image,x,y); //fabs(image.get(x,y)-image.get(x-1,y));
+	  temp.set(x,y,1.0);
+	  edge_points++;
+	}
+      }
+      
+    }
+  }
+  
+  
+  for(int y=1 ; y < ny ; y++){
+    
+    bool edge_found = false;
+    
+    for(int x=1 ; x < nx ; x++){
+
+      if(edge_found==false && image.get(x,y)>threshold ){
+	edge_found = true;
+	//image.set(x,y,60000);
+	if(temp.get(x,y)==0.0){
+	  value += sgrad(image,x,y); //fabs(image.get(x,y)-image.get(x,y-1));
+	  temp.set(x,y,1.0);
+	  edge_points++;
+	}
+      }
+
+    }
+    
+    edge_found = false;
+    
+    for(int x=nx-1 ; x > 0 ; x--){
+      
+      if(edge_found==false && image.get(x,y)>threshold ){
+      	edge_found = true;
+	//if(edge_found==true && image.get(x,y)<threshold ){
+	//edge_found = false;
+	//image.set(x,y,60000);
+	if(temp.get(x,y)==0.0){
+	  value += sgrad(image,x,y); //fabs(image.get(x,y)-image.get(x,y-1));
+	  temp.set(x,y,1.0);
+	  edge_points++;
+	}
+      }
+      
+    } 
+  }
+
+  static int counter = 0;
+  char buf[80];
+  sprintf(buf,"edges_%i.tiff",counter);
+  write_image(buf,temp);
+  counter++;
+
+  image.copy(temp);
+  
+  return value/edge_points;  
+}
 
 //no good
 double calculate_mean_difference(Double_2D & image){
--- cxs_software/src/utils.h
+++ cxs_software/src/utils.h
@@ -7,6 +7,7 @@ class Complex_2D;
 void crop(Double_2D & image, Double_2D & new_image, int x_start, int y_start);
 void rescale(Double_2D & image, double scale);
 
+double edges(Double_2D & image);
 double line_out(Double_2D & image);
 double calculate_high_frequency_ratio(Double_2D & image);
 double calculate_image_entropy(Double_2D & image);
@@ -21,6 +22,8 @@ double diff_of_squares(Double_2D & image1, Double_2D & image2);
 double simple(Double_2D & image, double scale);
 double sobel_gradient(Double_2D & image);
 double laplace_gradient(Double_2D & image);
+double edge_grad(Double_2D & image, Double_2D & mask);
+double calculate_image_entropy_2(Double_2D & image);
 
 #endif
 
